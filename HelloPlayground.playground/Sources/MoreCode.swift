1900bd3a1e8d7c910e75b364a39daae39610d52d
Christian Gröling <ch.groeling@icloud.com>
2020-03-21 11:54:02 +0100

    Added comment

--- HelloPlayground.playground/Sources/MoreCode.swift
+++ HelloPlayground.playground/Sources/MoreCode.swift
@@ -1,6 +1,16 @@
 import Foundation
 
-
+/// Test Brief
+///
+/// Test Description 123
+///
+///     Test Code
+///     fejjfeif
+///
+/// [...]
+///
+/// - Parameter i: Parameter
+///
 public func DoubleTheNumber ( i:Int ) -> Int {
     let x = i * 2
     return x
