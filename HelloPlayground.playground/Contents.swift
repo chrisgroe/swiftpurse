31cc86ce365b7767400db40b92c5f08db6b01dae
Christian Gröling <ch.groeling@icloud.com>
2020-03-21 11:37:41 +0100

    Added example playgroudn

---
+++ HelloPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//:# Hello Playground
+import Cocoa
+/*:
+ Markup Comment
+ * first
+ * second
+ * **bold** *italic*
+ */
+
+// comment
+var str = "Hello, playground"
+DoubleTheNumber(i:6)
+
+//:
---
+++ HelloPlayground.playground/Sources/MoreCode.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+
+public func DoubleTheNumber ( i:Int ) -> Int {
+    let x = i * 2
+    return x
+}
+
